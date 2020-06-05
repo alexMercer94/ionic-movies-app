92d4b1729b357d3ce26cb1b041d815f181ee9ba3
alexMercer94 <alex.c.valera94@hotmail.com>
2020-06-04 19:12:31 -0500

    Last details in movie details view

--- src/components/detail/detail.component.ts
+++ src/components/detail/detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import { MoviesService } from 'src/services/movies/movies.service';
 
 @Component({
@@ -9,8 +10,15 @@ import { MoviesService } from 'src/services/movies/movies.service';
 export class DetailComponent implements OnInit {
     @Input() id;
     movie: IGetMovieDetail = {};
+    actors: Crew[];
+    hide = 150;
+    slideOptActors = {
+        slidesPerView: 3.3,
+        freeMode: true,
+        spaceBetween: -5,
+    };
 
-    constructor(private moviesService: MoviesService) {}
+    constructor(private moviesService: MoviesService, private modalCtrl: ModalController) {}
 
     ngOnInit() {
         this.getDetailMovie();
@@ -25,7 +33,11 @@ export class DetailComponent implements OnInit {
 
     public getCredits(): void {
         this.moviesService.getMovieCredits(this.id).subscribe((res: IGetCredits) => {
-            console.log(res);
+            this.actors = res.crew;
         });
+    }
+
+    public back(): void {
+        this.modalCtrl.dismiss();
     }
 }
